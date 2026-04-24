325380a0290e4695dee86e330c2faff768058929
Malatesh Basavaraja Sunkada <malateshbsunkad03@gmail.com>
2026-04-24 09:32:39 +0530

    Add API section comment in main.py

--- app/main.py
+++ app/main.py
@@ -12,6 +12,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
 from pydantic import BaseModel
 
+#---API---
 app = FastAPI()
 
 # --- CONFIGURATION ---
